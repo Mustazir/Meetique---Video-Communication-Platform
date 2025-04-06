6af025650c7c6e6bd56d63f18cf698a0717b1658
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-04-06 22:02:24 +0600

    authentication error

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,50 +1,50 @@
 
-// import 'package:Meetique/resources/auth_methods.dart';
-// import 'package:Meetique/widgets/custom_button.dart';
-// import 'package:flutter/material.dart';
+import 'package:Meetique/resources/auth_methods.dart';
+import 'package:Meetique/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
 
-// class LoginScreen extends StatefulWidget {
-//   const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
-//   @override
-//   State<LoginScreen> createState() => _LoginScreenState();
-// }
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
 
-// class _LoginScreenState extends State<LoginScreen> {
-//   final AuthMethods _authMethods = AuthMethods();
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthMethods _authMethods = AuthMethods();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           const Text(
-//             'Start or join a meeting',
-//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 38.0),
-//             child: Image.asset('assets/images/onboarding.jpg'),
-//           ),
-//           CustomButton(
-//             text: 'Google Sign In',
-//             onPressed: () async {
-//               bool res = await _authMethods.signInWithGoogle(context);
-//               if (res) {
-//                 Navigator.pushNamed(context, '/home');
-//               }
-//             },
-//           ),
-//           const SizedBox(height: 20), // Add some space between the buttons
-//           CustomButton(
-//             text: 'Go to Home',
-//             onPressed: () {
-//               Navigator.pushNamed(context, '/home');
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38.0),
+            child: Image.asset('assets/images/onboarding.jpg'),
+          ),
+          CustomButton(
+            text: 'Google Sign In',
+            onPressed: () async {
+              bool res = await _authMethods.signInWithGoogle(context);
+              if (res) {
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          ),
+          const SizedBox(height: 20), // Add some space between the buttons
+          CustomButton(
+            text: 'Go to Home',
+            onPressed: () {
+              Navigator.pushNamed(context, '/home');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
